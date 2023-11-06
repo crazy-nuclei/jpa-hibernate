6d99bef23566e3495adb7b7c2c37f9cda9c8350a
Aayush Agarwal <crazy_nuclei@localhost>
2023-11-06 23:09:37 +0530

    Manipulating data using spring JDBC

---
+++ src/main/java/com/example/jpahibernate/demoJPAHibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -0,0 +1,27 @@
+package com.example.jpahibernate.demoJPAHibernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.example.jpahibernate.demoJPAHibernate.course.Course;
+
+@Component
+public class CourseJdbcCommandLineRunner implements CommandLineRunner{
+	
+	@Autowired
+	private CourseJdbcRepository repository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		repository.insert(new Course(1, "OS", "AA"));
+		repository.insert(new Course(2, "CN", "AY"));
+		repository.insert(new Course(3, "DBMS", "AU"));
+		
+		repository.deleteById(1);
+		
+		System.out.println(repository.selectById(2));
+	}
+
+}
